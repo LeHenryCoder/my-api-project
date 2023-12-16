013337c63b7c8bc1f23bea5acd9e41a80f46eddb
LeHenryCoder <lao.henry.era@gmail.com>
2023-12-16 23:30:04 +0000

    Added Terraform configuration file

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "us-east-2"  # Replace with the appropriate region
+}
+
+
+
+resource "aws_instance" "api_server" {
+  ami           = "ami-048e636f368eb3006"  # Your selected AMI ID
+  instance_type = "t2.micro"  # Choose an instance type that suits your needs
+
+
+
+  tags = {
+    Name = "API Server"
+  }
+}
